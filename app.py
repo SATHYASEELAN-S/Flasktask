1f5aa1dd66b7531fd084a72a5e22c147a25fcd4e
SATHYASEELAN-S <sathyaseelan0@yahoo.com>
2024-04-11 22:48:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask, jsonify,render_template,url_for,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:@localhost/employee'
+db=SQLAlchemy(app)
+
+
+class Employee(db.Model):
+
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(200),nullable=False)
+    age=db.Column(db.Integer,nullable=False)
+    gender=db.Column(db.String(10),nullable=False)
+    Designation=db.Column(db.String(100),nullable=False)
+
+
+    def __repr__(self):
+        return f"<Employee(id={self.id}, name={self.name}, age={self.age}, gender={self.gender}, Designation={self.Designation})>"
+    
+
+
+
+
+
+
+
+
+@app.route("/",methods=['GET','POST'])
+def home():
+
+    if request.method=="POST":
+        name=request.form.get("name")
+        age=request.form.get("age")
+        gender=request.form.get("gender")
+        des=request.form.get("designation")
+
+        
+        details=Employee(name=name,age=age,gender=gender,Designation=des)
+
+        try:
+            db.session.add(details)
+            db.session.commit()
+            redirect("")
+        except:
+            return "Error"
+        print(name,age,gender,des)
+        redirect("/")
+
+    details=Employee.query.all()
+    print(details)
+    return render_template("index.html",details=details)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    emp=Employee.query.get_or_404(id)
+    try:
+        db.session.delete(emp)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "error"
+
+@app.route("/upd/<int:id>",methods=['post'])
+def up(id):
+    print("up in")
+    emp=Employee.query.get_or_404(id)
+    if request.method=='POST':
+        print("emp",emp)
+        emp.name=request.form["name"]
+        emp.age=request.form["age"]
+        emp.gender=request.form["gender"]
+        emp.Designation=request.form["designation"]
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "update Failed"
+    
+    
+
+@app.route("/update/<int:id>",methods=['POST','GET'])
+def update(id):
+    emp=Employee.query.get_or_404(id)
+
+    emp_data = {
+        'id': emp.id,
+        'name': emp.name,
+        'age': emp.age,
+        'des': emp.Designation,
+        'gender': emp.gender,
+       
+    }
+    
+        
+
+    return jsonify({'message': 'Employee updated successfully','emp':emp_data})
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,48 @@
+
+  
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        id="";
+        var editLinks = document.querySelectorAll('.editLink');
+        editLinks.forEach(function (link) {
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+                var empId = this.getAttribute('data-emp-id');
+                
+
+                $.ajax({
+                    type: 'GET',
+                    url: '/update/' + empId,
+                    
+                    success: function(response) {
+                        
+                        console.log(response.emp.des);
+                        id=response.emp.id;
+                        $('#updateemp').modal('show');
+                        document.getElementById('vname').value = response.emp.name;
+        document.getElementById('vage').value =response.emp.age;
+        document.getElementById('vgender').value = response.emp.gender;
+        document.getElementById('vdesignation').value =response.emp.des;
+        document.getElementById('updateform').action = '/upd/' + response.emp.id;
+                    },
+                    error: function(xhr, status, error) {
+                      
+                        console.error(xhr.responseText);
+                    }
+                });
+
+
+
+            });
+        });
+          document.getElementById('submitButton').addEventListener('click', function (event) {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+        console.log("form in ");
+
+        document.getElementById('updateform').action = '/upd/' + id;
+
+        document.getElementById('updateform').submit();
+    });
+    });
